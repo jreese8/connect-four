575fb5b7ef41b4a70ebd142de9b4ee3ae492118f
Johnathan Reese <JohnathanReese8@gmail.com>
2023-01-24 14:37:35 -0500

    Fixed mistakes and finished

--- connectFour.js
+++ connectFour.js
@@ -4,6 +4,7 @@ var currPlayer = playerRed;
 
 var gameOver =false;
 var board;
+var currColumns; //keeps track of which row each column is at.
 
 var rows = 6;
 var columns = 7;
@@ -15,18 +16,118 @@ window.onload = function() {
 //populates tiles within board
 function setGame() {
     board = [];
+    currColumns = [5, 5, 5, 5, 5, 5, 5];
 
     for (let r = 0; r < rows; r++) {
-        for (let c=0; c < columns; c++)
-        //JS
-        rows.push(' ');
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            // JS
+            row.push(' ');
 
-        //HTML
-        //<div id ="0-0" class="tile"></div> this is appending <div id = "board"> in HTML
-        let tile = document.createElement("div");
-        tile.id = r.toString() + "-" + c.toString();
-        tile.classList.add("tile");
-        document.getElementById("board").append(tile);
+            // HTML
+            //<div id ="0-0" class="tile"></div> this is appending <div id = "board"> in HTML
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
+        }
+        board.push(row);
     }
-    board.push(row);
+}
+
+function setPiece() {
+    if (gameOver) {
+        return;
+    }
+    
+    //get coords of that tile clicked
+    let coords = this.id.split("-"); //"0-0" -> ["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    // figures out which row the current column should be on
+    r = currColumns[c];
+
+    if (r < 0) {    // board[r][c] != ' '
+        return;
+    }
+
+    board[r][c] = currPlayer; //update JS board
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerYellow;
+    }
+
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = playerRed;
+    }
+
+    r -= 1; //updating the row height for the column
+    currColumns[c] = r; //update the array
+
+    checkWinner();
+
+}
+
+function checkWinner() {
+    //horizontally
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++){
+           if (board[r][c] != ' ') {
+               if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                   setWinner(r, c);
+                   return;
+               }
+           }
+        }
+   }
+
+   // vertically
+   for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+        if (board[r][c] != ' ') {
+            if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                setWinner(r, c);
+                return;
+            }
+        }
+    }
+}
+
+    // anti-diagonally, top left to bttm right
+for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+        if (board[r][c] != ' ') {
+            if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                setWinner(r, c);
+                return;
+            }
+        }
+    }
+}
+
+// diagonally, top right to bttm left
+for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+        if (board[r][c] != ' ') {
+            if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                setWinner(r, c);
+                return;
+            }
+        }
+    }
+}
+}
+
+function setWinner(r, c) {
+let winner = document.getElementById("winner");
+if (board[r][c] == playerRed) {
+    winner.innerText = "Red Wins";             
+} else {
+    winner.innerText = "Yellow Wins";
+}
+gameOver = true;
 }
